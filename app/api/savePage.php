09d2a0e04cbf7e5fff8729a4c5f376a7e4a50302
Евгений-123 <up.krd2018@gmail.com>
2021-12-03 15:59:29 +0300

    #5

--- app/api/savePage.php
+++ app/api/savePage.php
@@ -4,7 +4,7 @@ $_POST = json_decode(file_get_contents("php://input"), true);
 $file = "../../" . $_POST["pageName"];
 $newHTML = $_POST["html"];
 
-if ($newFile && $file) {
+if ($newHTML && $file) {
     file_put_contents($file, $newHTML);
 } else {
     header("HTTP/1.0 400 Bad Request");
--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -4,31 +4,35 @@ import React, {Component} from 'react';
 import "../../helpers/iframeLoader";
 import DOMHelper from "../../helpers/dom-helper";
 import EditorText from "../editor-text";
-import UIKit from "uikit"
+import UIkit from "uikit";
+import Spinner from '../spinner'
 
 
 export default class Editor extends Component {
     constructor() {
         super();
-        this.currentPage = "index.html"
+        this.currentPage = "index.html";
         this.state = {
             pageList: [],
-            newPageName: ""
+            newPageName: "",
+            loading: true
         }
-        this.createNewPage = this.createNewPage.bind(this)
+        this.createNewPage = this.createNewPage.bind(this);
+        this.isLoading = this.isLoading.bind(this);
+        this.isLoaded = this.isLoaded.bind(this);
     }
 
     componentDidMount() {
-        this.init(this.currentPage)
+        this.init(this.currentPage);
     }
 
     init(page) {
-        this.iframe = document.querySelector('iframe')
-        this.open(page);
+        this.iframe = document.querySelector('iframe');
+        this.open(page, this.isLoaded);
         this.loadPageList();
     }
 
-    open(page) {
+    open(page, cb) {
         this.currentPage = page;
 
         axios
@@ -44,49 +48,44 @@ export default class Editor extends Component {
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing())
             .then(() => this.injectStyles())
-
+            .then(cb);
     }
 
-    save() {
-       const newDom = this.virtualDom.cloneNode(this.virtualDom)
-        DOMHelper.unwrapTextNodes(newDom)
-        const html = DOMHelper.serializeDOMToString(newDom)
+    save(onSuccess, onError) {
+        this.isLoading();
+        const newDom = this.virtualDom.cloneNode(this.virtualDom);
+        DOMHelper.unwrapTextNodes(newDom);
+        const html = DOMHelper.serializeDOMToString(newDom);
         axios
-            .post("./api/savePage.php", {pageName: this.currentPage, html })
-    }
-
-
+            .post("./api/savePage.php", {pageName: this.currentPage, html})
+            .then(onSuccess)
+            .catch(onError)
+            .finally(this.isLoaded);
+    };
 
     enableEditing() {
-       this.iframe.contentDocument.body.querySelectorAll('text-editor').forEach(element => {
-           const id = element.getAttribute('nodeid')
-           const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`)
+        this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
+            const id = element.getAttribute("nodeid");
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
 
-           new EditorText(element, virtualElement)
-
-       })
+            new EditorText(element, virtualElement);
+        });
     }
 
     injectStyles() {
-        const style = this.iframe.contentDocument.createElement('style')
+        const style = this.iframe.contentDocument.createElement("style");
         style.innerHTML = `
             text-editor:hover {
-               outline: 3px solid orange;
-               outline-offset: 8px;
+                outline: 3px solid orange;
+                outline-offset: 8px;
             }
             text-editor:focus {
-               outline: 3px solid red;
-               outline-offset: 8px;
+                outline: 3px solid red;
+                outline-offset: 8px;
             }
         `;
-        this.iframe.contentDocument.head.appendChild(style)
-    }
-
-
-
-
-
-
+        this.iframe.contentDocument.head.appendChild(style);
+    };
 
     loadPageList() {
         axios
@@ -98,37 +97,64 @@ export default class Editor extends Component {
         axios
             .post("./api/createNewPage.php", {"name": this.state.newPageName})
             .then(this.loadPageList())
-            .catch(() => alert("Страница уже существует"))
+            .catch(() => alert("Страница уже существует!"));
     }
 
     deletePage(page) {
         axios
             .post("./api/deletePage.php", {"name": page})
             .then(this.loadPageList())
-            .catch(() => alert("Страницы не существует"))
+            .catch(() => alert("Страницы не существует!"));
+    }
+
+    isLoading() {
+        this.setState({
+            loading: true
+        })
+    }
+
+    isLoaded() {
+        this.setState({
+            loading: false
+        })
     }
 
     render() {
+        const {loading} = this.state;
+        const modal = true;
+        let spinner;
+        
+        loading ? spinner = <Spinner active/> : spinner = <Spinner />
+
         return (
             <>
-                <iframe src={this.currentPage} frameBorder="0"/>
+                <iframe src={this.currentPage} frameBorder="0"></iframe>
+                
+                {spinner}
+
                 <div className="panel">
-                    <button className="uk-button uk-button-primary" uk-toggle="target: #modal-example">Опубликовать</button>
+                    <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
                 </div>
-
-                    <div id="modal-save" uk-modal="true">
-                        <div className="uk-modal-dialog uk-modal-body">
-                            <h2 className="uk-modal-title">Сохранение</h2>
-                            <p>Вы действительно хотите сохранить изменения?</p>
-                            <p className="uk-text-right">
-                                <button className="uk-button uk-button-default uk-modal-close" type="button">Отменить</button>
-                                <button className="uk-button uk-button-primary" type="button">Опубликовать</button>
-                            </p>
-                        </div>
+                
+                <div id="modal-save" uk-modal={modal.toString()} container="false">
+                    <div className="uk-modal-dialog uk-modal-body">
+                        <h2 className="uk-modal-title">Сохранение</h2>
+                        <p>Вы действительно хотите сохранить изменения?</p>
+                        <p className="uk-text-right">
+                            <button className="uk-button uk-button-default uk-modal-close" type="button">Отменить</button>
+                            <button 
+                                className="uk-button uk-button-primary uk-modal-close" 
+                                type="button"
+                                onClick={() => this.save(() => {
+                                    UIkit.notification({message: 'Успешно сохранено', status: 'success'})
+                                },
+                                () => {
+                                    UIkit.notification({message: 'Ошибка сохранения', status: 'danger'})
+                                })}>Опубликовать</button>
+                        </p>
                     </div>
+                </div>
             </>
-
-
-        )
+        );
     }
 }
